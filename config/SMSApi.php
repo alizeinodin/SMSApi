287babd2e96e6068d0533b71ca35bc103d5532f0
alizeinodin <alizeinodin79@gmail.com>
2023-08-23 17:11:36 +0330

    Add env variable for ssl verification mode

--- config/SMSApi.php
+++ config/SMSApi.php
@@ -2,5 +2,6 @@
 return [
     'api_key' => env('SMSAPI_API_KEY', "null"),
     'secret_key' => env('SMSAPI_SECRET_KEY'),
-    'line_number' => env('SMSAPI_LINE_NUMBER')
+    'line_number' => env('SMSAPI_LINE_NUMBER'),
+    'ssl_verifier' => env('SMSAPI_SSL', true) === 'false' ? false : true,
 ];
